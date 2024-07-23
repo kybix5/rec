c30878277661cd5049c5489622e755552dcae1c0
kybix <galiaf555@mail.ru>
2024-07-23 14:21:00 +0300

    good vlc

--- lib/vlc.dart
+++ lib/vlc.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_vlc_player/flutter_vlc_player.dart';
 
-
 import 'main.dart';
 import 'tree.dart';
 import 'news.dart';
@@ -20,68 +19,14 @@ class MyThirdPage extends StatefulWidget {
 class _MyThirdPageState extends State<MyThirdPage> {
   // Future<void> initializePlayer() async {}
   bool _isPlaying = true;
-  String _icon = "play_arrow";
-
-  void naveel_home() {
-    cout_men = 0;
-    Navigator.push(context,
-        MaterialPageRoute(builder: (context) => MyHomePage(title: 'Анчих')));
-  }
-
-  void naveel_news() {
-    cout_men = 3;
-    Navigator.push(
-        context,
-        MaterialPageRoute(
-            builder: (context) => MySecondPage(title: 'Новости')));
-  }
-
-  void naveel_camera() {
-    cout_men = 1;
-    Navigator.push(context,
-        MaterialPageRoute(builder: (context) => MyThirdPage(title: 'Камера')));
-  }
-
-  void naveel_tree() {
-    cout_men = 2;
-    Navigator.push(
-        context,
-        MaterialPageRoute(
-            builder: (context) => MyFourPage(title: 'Генеалогическое древо')));
-  }
-
-  void naveel_weather() {
-    cout_men = 4;
-    Navigator.push(
-        context,
-        MaterialPageRoute(
-            builder: (context) => MyWeatherPage(title: 'Погода')));
-  }
-
-  void onItemTapped(index) {
-    cout_men = index;
-
-    if (index == 0) {
-      naveel_home();
-    } else if (index == 1) {
-      naveel_camera();
-    } else if (index == 2) {
-      naveel_tree();
-    } else if (index == 3) {
-      naveel_news();
-    } else if (index == 4) {
-      naveel_weather();
-    }
-    setState(() {});
-  }
 
   final VlcPlayerController _videoPlayerController =
       VlcPlayerController.network(
-    //'https://media.w3.org/2010/05/sintel/trailer.mp4',
-    'rtsp://46.16.226.6:554/user=user&password=&channel=1&stream=0',
-    //hwAcc: HwAcc.full,
+    'https://media.w3.org/2010/05/sintel/trailer.mp4',
+    //'rtsp://46.16.226.6:554/user=user&password=&channel=1&stream=0',
+    hwAcc: HwAcc.full,
     autoPlay: true,
-    // options: VlcPlayerOptions(),
+    options: VlcPlayerOptions(),
   );
 
   late Color _buttonColor_play = Colors.green;
@@ -133,7 +78,6 @@ class _MyThirdPageState extends State<MyThirdPage> {
         ],
       ),
       bottomNavigationBar: NavigationExample(),
-
     );
   }
 }
