ed92e619a2f8f0948ef75116d31d75e23b2cf257
kybix5 <galiaf555@mail.ru>
2024-10-13 13:41:40 +0300

    title

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Bottom Navigation Bar Example',
+      title: 'Анчих1',
       home: BottomNavExample(),
     );
   }
