f39f6a58b301d07ef8be44802ebabaab62cec53c
kybix5 <galiaf555@mail.ru>
2024-10-27 12:40:24 +0300

    Выбор двух родителей

--- lib/tree.dart
+++ lib/tree.dart
@@ -8,6 +8,7 @@ String arrayObjsT = '{"table": []}';
 var tableObjsJson = jsonDecode(arrayObjsT)['table'] as List;
 var tableTemp = jsonDecode(arrayObjsT)['table'] as List;
 var obj_person = jsonDecode(arrayObjsT)['table'] as List;
+var obj_femaly = jsonDecode(arrayObjsT)['table'] as List;
 
 class TreeScreenWidget extends StatefulWidget {
   const TreeScreenWidget({super.key});
@@ -28,12 +29,8 @@ class _TreeScreenWidgetState extends State<TreeScreenWidget> {
     for (int i = 0; i < tableObjsJson.length; i++) {
       myString = tableObjsJson[i]["name"];
       if (myString.contains(_search.text)) {
-        tableTemp.add({
-          "n_id": tableObjsJson[i]["n_id"],
-          "name": tableObjsJson[i]["name"],
-          "age": tableObjsJson[i]["age"],
-          "birthday": tableObjsJson[i]["birthday"]
-        });
+        tableTemp.add(
+            {"n_id": tableObjsJson[i]["n_id"], "name": tableObjsJson[i]["name"], "age": tableObjsJson[i]["age"], "birthday": tableObjsJson[i]["birthday"]});
       }
     }
     print(tableTemp);
@@ -58,9 +55,7 @@ class _TreeScreenWidgetState extends State<TreeScreenWidget> {
               decoration: InputDecoration(
                 fillColor: Colors.white,
                 filled: true,
-                border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: BorderSide.none),
+                border: OutlineInputBorder(borderRadius: BorderRadius.circular(10), borderSide: BorderSide.none),
                 hintText: 'Поиск..',
                 hintStyle: const TextStyle(color: Colors.grey, fontSize: 10),
                 prefixIcon: Container(
@@ -88,8 +83,7 @@ class _ListBuilderState extends State<ListBuilderState> {
     if (tableObjsJson.length > 1) {
       return Future.value("Data download"); // return your response
     } else {
-      var request =
-          await HttpClient().getUrl(Uri.parse('https://anchih.e-rec.ru/api'));
+      var request = await HttpClient().getUrl(Uri.parse('https://anchih.e-rec.ru/api/'));
       // sends the request
       var response = await request.close();
       // transforms and prints the response
@@ -124,8 +118,7 @@ class _ListBuilderState extends State<ListBuilderState> {
                         title: Text(tableTemp[index]["name"]),
                         // ignore: prefer_interpolation_to_compose_strings
                         subtitle: Text(
-                          "${"Возрасть:" + tableTemp[index]["age"]} год.рождения:" +
-                              tableTemp[index]["birthday"],
+                          "${"Возрасть:" + tableTemp[index]["age"]} год.рождения:" + tableTemp[index]["birthday"],
                         ),
                         trailing: Row(
                           mainAxisSize: MainAxisSize.min,
@@ -136,8 +129,7 @@ class _ListBuilderState extends State<ListBuilderState> {
                                 showDialog(
                                     context: context,
                                     builder: (BuildContext context) {
-                                      return add_person(
-                                          tableTemp[index]["n_id"]);
+                                      return add_person(tableTemp[index]["name"], tableTemp[index]["n_id"]);
                                     });
                               },
                             ),
@@ -147,8 +139,7 @@ class _ListBuilderState extends State<ListBuilderState> {
                                 showDialog(
                                     context: context,
                                     builder: (BuildContext context) {
-                                      return _alert_list(
-                                          tableTemp[index]["n_id"]);
+                                      return _alert_list(tableTemp[index]["n_id"]);
                                     });
                               },
                             ),
@@ -170,8 +161,7 @@ class _alert_list extends StatelessWidget {
   _alert_list(this.id_person);
 
   Future<String> get_person() async {
-    var request = await HttpClient()
-        .getUrl(Uri.parse('https://anchih.e-rec.ru/api/person?id=$id_person'));
+    var request = await HttpClient().getUrl(Uri.parse('https://anchih.e-rec.ru/api/person?id=$id_person'));
     // sends the request
     var response = await request.close();
     // transforms and prints the response
@@ -219,8 +209,7 @@ class _alert_list extends StatelessWidget {
                                       showDialog(
                                           context: context,
                                           builder: (BuildContext context) {
-                                            return _alert_list(
-                                                obj_person[i]["n_id"]);
+                                            return _alert_list(obj_person[i]["n_id"]);
                                           });
                                     },
                                   ),
@@ -245,15 +234,17 @@ class _alert_list extends StatelessWidget {
 
 class add_person extends StatefulWidget {
   String id_person;
-  add_person(this.id_person);
+  String id_name;
+  add_person(this.id_name, this.id_person);
 
   @override
-  _add_person createState() => _add_person(id_person);
+  _add_person createState() => _add_person(id_name, id_person);
 }
 
 class _add_person extends State<add_person> {
   String id_person;
-  _add_person(this.id_person);
+  String id_name;
+  _add_person(this.id_name, this.id_person);
 
   final TextEditingController nameController = TextEditingController();
   final TextEditingController surnameController = TextEditingController();
@@ -263,35 +254,42 @@ class _add_person extends State<add_person> {
   final TextEditingController _ageController = TextEditingController();
   DateTime? _selectedDate;
 
-  String? selectedRelation; // Для хранения выбранного значения
+  String? selectedFams; // Для хранения выбранного значения
 
+  List<List<String>> fams = [];
+
+  String? selectedRelation; // Для хранения выбранного значения
   List<String> relations = [
-    'Семья',
-    'Друг',
-    'Коллега',
-    'Знакомый',
+    'Сын',
+    'Дочь',
   ];
 
-  Future<String> get_person() async {
-    var request = await HttpClient()
-        .getUrl(Uri.parse('https://anchih.e-rec.ru/api/person?id=$id_person'));
+  Future<String> getFamaly() async {
+    var request = await HttpClient().getUrl(Uri.parse('https://anchih.e-rec.ru/api/person/getfamaly.php?id=$id_person'));
     // sends the request
     var response = await request.close();
     // transforms and prints the response
     await for (var contents in response.transform(const Utf8Decoder())) {
-      obj_person = jsonDecode(contents)['person'] as List;
+      obj_femaly = jsonDecode(contents)['person'] as List;
+      // Преобразуем данные в список строк
+      fams = obj_femaly.map((item) => [item['name'].toString(), item['FAMC'].toString()]).toList();
     }
     return Future.value("Data download"); // return your response
   }
 
-  Future<void> _sendDataToServer(
-      String name, String surname, String age, String relation) async {
+  Future<void> _sendDataToServer(String id_famaly, String name, String surname, String age, String relation) async {
+    final url = Uri.parse('https://anchih.e-rec.ru/api/person/add_person.php'); // Замените на ваш URL
+
     final response = await http.post(
-      Uri.parse('https://your-api-url.com/endpoint'), // Замените на ваш URL
+      url,
       headers: <String, String>{
         'Content-Type': 'application/json; charset=UTF-8',
       },
       body: jsonEncode(<String, String>{
+        'id_person': id_person,
+        'person_name': id_name,
+        'id_famaly': id_famaly,
+        //'famaly_name': famaly_name,
         'name': name,
         'surname': surname,
         'age': age,
@@ -302,10 +300,43 @@ class _add_person extends State<add_person> {
     if (response.statusCode == 200) {
       // Успешно отправлено
       print('Данные успешно отправлены');
+      // Закрываем форму
+      try {
+        final responseJson = jsonDecode(response.body);
+        if (responseJson['answer'] == 'ok') {
+          Navigator.of(context).pop();
+        } else {
+          print('Ошибка при отправке данных: ${responseJson}');
+          _showErrorDialog(responseJson['answer']);
+        }
+      } catch (e) {
+        print('Ошибка декодирования JSON: $e');
+        _showErrorDialog('Ошибка декодирования JSON: $e');
+      }
     } else {
       // Ошибка при отправке
       print('Ошибка при отправке данных: ${response.statusCode}');
+      // Показываем сообщение об ошибке
+      _showErrorDialog('Ошибка при отправке данных: ${response.statusCode}');
     }
+  }
+
+  void _showErrorDialog(String message) {
+    showDialog(
+      context: context,
+      builder: (ctx) => AlertDialog(
+        title: Text('Ответ сервера'),
+        content: Text(message),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.of(ctx).pop(); // Закрыть диалог
+            },
+            child: Text('OK'),
+          ),
+        ],
+      ),
+    );
   }
 
   @override
@@ -324,7 +355,7 @@ class _add_person extends State<add_person> {
       if (pickedDate == null) return;
       setState(() {
         _selectedDate = pickedDate;
-        _ageController.text = DateFormat('yyyy').format(_selectedDate!);
+        _ageController.text = DateFormat('d MMMM yyyy').format(_selectedDate!);
       });
     });
   }
@@ -332,7 +363,7 @@ class _add_person extends State<add_person> {
   @override
   Widget build(BuildContext context) {
     return FutureBuilder<String>(
-        future: get_person(), // function where you call your api
+        future: getFamaly(), // function where you call your api
         builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
           // AsyncSnapshot<Your object type>
           if (snapshot.connectionState == ConnectionState.waiting) {
@@ -342,10 +373,31 @@ class _add_person extends State<add_person> {
               return Center(child: Text('Error: ${snapshot.error}'));
             } else {
               return AlertDialog(
-                title: Text('Введите данные'),
+                title: Column(
+                  children: [
+                    Text(id_name),
+                    Text(
+                      'Добавление детей',
+                      style: const TextStyle(fontSize: 12),
+                    ),
+                  ],
+                ),
                 content: SingleChildScrollView(
                   child: Column(
                     children: [
+                      DropdownButtonFormField<String>(
+                        value: selectedFams,
+                        decoration: InputDecoration(labelText: 'к супругу(е)'),
+                        items: fams.map((List<String> fam) {
+                          return DropdownMenuItem<String>(
+                            value: fam[1],
+                            child: Text(fam[0]),
+                          );
+                        }).toList(),
+                        onChanged: (String? newValue) {
+                          selectedFams = newValue;
+                        },
+                      ),
                       TextField(
                         controller: nameController,
                         decoration: InputDecoration(labelText: 'Имя'),
@@ -358,9 +410,8 @@ class _add_person extends State<add_person> {
                         controller: _ageController,
                         decoration: InputDecoration(labelText: 'Год рождения'),
                         keyboardType: TextInputType.number,
-                        readOnly: true, // Запретить ввод текста
-                        onTap:
-                            _presentDatePicker, // Открыть календарь при нажатии
+                        readOnly: true,
+                        onTap: _presentDatePicker,
                       ),
                       DropdownButtonFormField<String>(
                         value: selectedRelation,
@@ -374,7 +425,7 @@ class _add_person extends State<add_person> {
                         onChanged: (String? newValue) {
                           selectedRelation = newValue;
                         },
-                      )
+                      ),
                     ],
                   ),
                 ),
@@ -387,13 +438,40 @@ class _add_person extends State<add_person> {
                   ),
                   TextButton(
                     onPressed: () {
+                      if (selectedFams == null) {
+                        _showErrorDialog('Пожалуйста, выберите мать для ребенка');
+                        return;
+                      }
+                      // Проверка на заполненность полей
+                      if (nameController.text.isEmpty) {
+                        // Здесь можно показать сообщение об ошибке
+                        _showErrorDialog('Пожалуйста, введите имя');
+                        return;
+                      }
+                      if (surnameController.text.isEmpty) {
+                        _showErrorDialog('Пожалуйста, введите фамилию');
+                        return;
+                      }
+                      if (_ageController.text.isEmpty) {
+                        _showErrorDialog('Пожалуйста, выберите год рождения');
+                        return;
+                      }
+                      if (selectedRelation == null) {
+                        _showErrorDialog('Пожалуйста, выберите отношение');
+                        return;
+                      }
+
+                      // Если все проверки пройдены, отправляем данные на сервер
                       _sendDataToServer(
+                        selectedFams!,
                         nameController.text,
                         surnameController.text,
-                        ageController.text,
-                        relationController.text,
+                        _ageController.text,
+                        selectedRelation!,
                       );
-                      Navigator.of(context).pop();
+
+                      // Закрыть диалог после успешной отправки
+                      // Navigator.of(context).pop();
                     },
                     child: Text('Отправить'),
                   ),
